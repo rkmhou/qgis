89c642cc0e9216e59c388b9e05a3d30c4c064110
rkmhou <ramelmogar2025@gmail.com>
2025-05-26 15:27:58 +0800

    Add files via upload

---
+++ styles/POBLACION_ROADS_4_style.js
@@ -0,0 +1,31 @@
+var size = 0;
+var placement = 'point';
+
+var style_POBLACION_ROADS_4 = function(feature, resolution){
+    var context = {
+        feature: feature,
+        variables: {}
+    };
+    
+    var labelText = ""; 
+    var value = feature.get("");
+    var labelFont = "13.0px \'Open Sans\', sans-serif";
+    var labelFill = "#000000";
+    var bufferColor = "#e31a1c";
+    var bufferWidth = 3.0;
+    var textAlign = "left";
+    var offsetX = 0;
+    var offsetY = 0;
+    var placement = 'line';
+    if (feature.get("name") !== null) {
+        labelText = String(feature.get("name"));
+    }
+    var style = [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(255,255,255,1.0)', lineDash: null, lineCap: 'round', lineJoin: 'round', width: 1.3679999999999999}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+
+    return style;
+};
---
+++ styles/Place_5_style.js
@@ -0,0 +1,31 @@
+var size = 0;
+var placement = 'point';
+
+var style_Place_5 = function(feature, resolution){
+    var context = {
+        feature: feature,
+        variables: {}
+    };
+    
+    var labelText = ""; 
+    var value = feature.get("");
+    var labelFont = "13.0px \'Open Sans\', sans-serif";
+    var labelFill = "#000000";
+    var bufferColor = "#ffffff";
+    var bufferWidth = 3.0;
+    var textAlign = "left";
+    var offsetX = 0;
+    var offsetY = 0;
+    var placement = 'point';
+    if (feature.get("name") !== null) {
+        labelText = String(feature.get("name"));
+    }
+    var style = [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(35,35,35,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0.988}),fill: new ol.style.Fill({color: 'rgba(255,127,0,1.0)'}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+
+    return style;
+};
